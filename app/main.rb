6f6f5246a10257f426ed0d11df77f5d299437018
Kehvarl <Kehvarl@Kehvarl.com>
2022-01-05 19:15:14 -0700

    clicks and states

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,35 @@
+class Monster
+  def initialize x, y
+    @sprites = ['sprites/circle/red.png', 'sprites/circle/orange.png', 'sprites/circle/yellow.png',
+                'sprites/circle/green.png', 'sprites/circle/blue.png', 'sprites/circle/indigo.png',
+                'sprites/circle/violet.png', 'sprites/hexagon/indigo.png', 'sprites/hexagon/blue.png']
+    @current = 0
+    @x = x
+    @y = y
+    @size = 16
+
+    def sprite
+      @sprites[@current]
+    end
+  end
+
+  def click
+    @size += 1
+    if rand(10) >= 9
+      @current += 1
+    end
+  end
+
+  def tick args
+    if args.inputs.mouse.click
+      click
+    end
+    args.outputs.sprites << [@x, @y, @size, @size, sprite]
+  end
+end
+
 def tick args
-  args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
-  args.outputs.labels  << [640, 420, 'Join the Discord! http://discord.dragonruby.org', 5, 1]
-  args.outputs.sprites << [576, 280, 128, 101, 'dragonruby.png']
+  args.state.monster ||= Monster.new(64, 64)
+
+  args.state.monster.tick args
 end
