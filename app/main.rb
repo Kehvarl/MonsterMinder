e6dd495f55c51b0df5e1964f3dde165bdc1344cd
Kehvarl <Kehvarl@Kehvarl.com>
2022-01-11 19:37:57 -0700

    work on button class

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,40 @@
+class Button
+  def initialize x, y, text
+
+    @x = x
+    @y = y
+    @w = 5
+    @h = 10
+    @text = text
+  end
+
+  def render args
+    @w, @h = args.gtk.calcstringbox(@text, 2)
+    @w += 8
+    @h += 4
+
+    args.outputs.solids << [@x, @y, @w, @h, 128, 128, 128]
+    args.outputs.borders << [@x, @y, @w, @h, 0, 0, 0]
+    args.outputs.labels << [@x + (@w/2), @y + @h -2, @text, 2, 1, 0, 0, 0]
+  end
+
+  def render_pressed args
+    @w, @h = args.gtk.calcstringbox(@text, 2)
+    @w += 8
+    @h += 4
+
+    args.outputs.solids << [@x, @y, @w, @h, @r+128, @g+128, @b+128]
+    args.outputs.borders << [@x, @y, @w, @h, 0, 0, 0]
+    args.outputs.labels << [@x + (@w/2), @y + @h -2, @text, 2, 1, 0, 0, 0]
+  end
+
+  def clicked? args
+    cx = args.inputs.mouse.x
+    cy = args.inputs.mouse.y
+    (cx >= @x and cx <= @x+@w and cy >= @y and cy <= @y+@h)
+  end
+end
+
 class Monster
   def initialize x, y, type
     @x = x
@@ -7,6 +44,8 @@ class Monster
     @happiness = 128
     @tiredness = 0
     @sleep_time = -1
+    @play = Button.new(10, y-100, "Play")
+    @sleep = Button.new(70, y-100, "Sleep")
 
     def play
       if @tiredness < 100 and @sleep_time == -1
@@ -28,14 +67,8 @@ class Monster
       args.outputs.labels <<[x, y, "Happiness: #{@happiness}"]
       y -= 20
       args.outputs.labels <<[x, y, "Tiredness: #{@tiredness}"]
-      y -= 20
-      w,h = args.gtk.calcstringbox("Play", 1)
-      args.outputs.labels << [x + (w/2) + 5, y - h + h -2, "Play", 1, 1]
-      args.outputs.borders << [x, y - h, w+5, h, 0, 0, 0]
-
-      w,h = args.gtk.calcstringbox("Sleep", 1)
-      args.outputs.labels << [x + w + 10 + (w/2), y - h + h -2, "Sleep", 1, 1]
-      args.outputs.borders << [x + w + 5, y - h, w+10, h, 0, 0, 0]
+      @play.render args
+      @sleep.render args
 
     end
 
@@ -58,46 +91,9 @@ class Monster
   end
 end
 
-class Button
-  def initialize x, y, r, g, b, text, tr, tg, tb, args
-
-    @x = x
-    @y = y
-    @w, @h = args.gtk.calcstringbox(text, 2)
-    @w += 8
-    @h += 4
-    @r = r
-    @g = g
-    @b = b
-    @text = text
-    @tr = tr
-    @tg = tg
-    @tb = tb
-  end
-
-  def render args
-    args.outputs.solids << [@x, @y, @w, @h, @r, @g, @b]
-    args.outputs.borders << [@x, @y, @w, @h, 0, 0, 0]
-    args.outputs.labels << [@x + (@w/2), @y + @h -2, @text, 2, 1, @tr, @tg, @tb]
-  end
-
-  def render_pressed args
-    args.outputs.solids << [@x, @y, @w, @h, @r+128, @g+128, @b+128]
-    args.outputs.borders << [@x, @y, @w, @h, 0, 0, 0]
-    args.outputs.labels << [@x + (@w/2), @y + @h -2, @text, 2, 1, @tr, @tg, @tb]
-  end
-
-  def clicked? args
-    cx = args.inputs.mouse.x
-    cy = args.inputs.mouse.y
-    (cx >= @x and cx <= @x+@w and cy >= @y and cy <= @y+@h)
-  end
-
-end
 
 def tick args
   args.state.monster ||= Monster.new(10, 700, "Test")
-  args.state.button ||= Button.new(128, 64, 128, 128, 128, "Test", 0, 0, 0, args)
   args.state.turn_timer ||= 30
 
   args.state.turn_timer -= 1
@@ -107,15 +103,7 @@ def tick args
   end
 
   args.state.monster.render args
-  args.state.button.render args
 
-  if args.inputs.mouse.click or args.inputs.mouse.down
-
-    if args.state.button.clicked? args
-      args.state.monster.play
-      args.state.button.render_pressed args
-    end
-  end
 end
 
 #TODO
