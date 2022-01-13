c8a17a1e7cbecbd3fdf2d130a83ab2b5dd231823
Kehvarl <Kehvarl@Kehvarl.com>
2022-01-12 18:50:36 -0700

    some more button work

--- app/main.rb
+++ app/main.rb
@@ -1,14 +1,23 @@
 class Button
   def initialize x, y, text
-
     @x = x
     @y = y
     @w = 5
     @h = 10
     @text = text
+    @clicked = false
   end
 
   def render args
+    if @clicked
+      render_pressed args
+      @clicked = false
+    else
+      render_normal args
+    end
+  end
+
+  def render_normal args
     @w, @h = args.gtk.calcstringbox(@text, 2)
     @w += 8
     @h += 4
@@ -23,9 +32,16 @@ class Button
     @w += 8
     @h += 4
 
-    args.outputs.solids << [@x, @y, @w, @h, @r+128, @g+128, @b+128]
+    args.outputs.solids << [@x, @y, @w, @h, 255, 255, 255]
     args.outputs.borders << [@x, @y, @w, @h, 0, 0, 0]
     args.outputs.labels << [@x + (@w/2), @y + @h -2, @text, 2, 1, 0, 0, 0]
+  end
+
+  def click args
+    if args.inputs.mouse.button_left
+      @clicked =  clicked?(args)
+    end
+    return @clicked
   end
 
   def clicked? args
@@ -46,47 +62,55 @@ class Monster
     @sleep_time = -1
     @play = Button.new(10, y-100, "Play")
     @sleep = Button.new(70, y-100, "Sleep")
+  end
 
-    def play
-      if @tiredness < 100 and @sleep_time == -1
-        @happiness += rand(10)
-        @tiredness += 5
+  def play
+    if @tiredness < 100 and @sleep_time == -1
+      @happiness += rand(10)
+      @tiredness += 5
+    end
+  end
+
+  def sleep
+    if @sleep_time == -1
+      @sleep_time = 20
+    end
+  end
+
+  def render args
+    x, y = @x, @y
+    args.outputs.labels <<[x, y, @type]
+    y -= 20
+    args.outputs.labels <<[x, y, "Happiness: #{@happiness}"]
+    y -= 20
+    args.outputs.labels <<[x, y, "Tiredness: #{@tiredness}"]
+    @play.render args
+    @sleep.render args
+
+  end
+
+  def tick args
+    if @sleep_time > -1
+      @sleep_time -=5
+      if @sleep_time <= 0
+        @tiredness = 0
+        @sleep_time = -1
+      end
+    else
+      @happiness -= rand(3)
+      @tiredness += rand(2)
+
+      if @tiredness >= 20
+        sleep
       end
     end
 
-    def sleep
-      if @sleep_time == -1
-        @sleep_time = 20
-      end
+    if @play.click(args)
+      play
     end
 
-    def render args
-      x, y = @x, @y
-      args.outputs.labels <<[x, y, @type]
-      y -= 20
-      args.outputs.labels <<[x, y, "Happiness: #{@happiness}"]
-      y -= 20
-      args.outputs.labels <<[x, y, "Tiredness: #{@tiredness}"]
-      @play.render args
-      @sleep.render args
-
-    end
-
-    def tick
-      if @sleep_time > -1
-        @sleep_time -=5
-        if @sleep_time <= 0
-          @tiredness = 0
-          @sleep_time = -1
-        end
-      else
-        @happiness -= rand(3)
-        @tiredness += rand(2)
-
-        if @tiredness >= 20
-          sleep
-        end
-      end
+    if @sleep.click(args)
+      sleep
     end
   end
 end
@@ -99,7 +123,7 @@ def tick args
   args.state.turn_timer -= 1
   if args.state.turn_timer <= 0
     args.state.turn_timer = 30
-    args.state.monster.tick
+    args.state.monster.tick args
   end
 
   args.state.monster.render args
